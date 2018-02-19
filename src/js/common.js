c6d299306e63d04da9ddaaa066b1551e933943e1
belaevslava <belaevslava@gmail.com>
2018-02-19 21:51:50 +0200

    fix bugs

--- src/js/common.js
+++ src/js/common.js
@@ -1,2 +1,16 @@
-feather.replace();
-AOS.init();
+(function($) {
+    feather.replace();
+    AOS.init();
+
+    $('.carousel').carousel({
+        interval: false
+    });
+
+    $(".carousel").swipe({
+        swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+            if (direction == 'left') $(this).carousel('next');
+            if (direction == 'right') $(this).carousel('prev');
+        },
+        allowPageScroll: "vertical"
+    });
+})(jQuery);

73f7d8ff7ed8542e1ad867323a2c5803cdeb3cac
belaevslava <belaevslava@gmail.com>
2018-02-26 23:41:36 +0200

    fixed animation

--- src/js/pages/game-room.js
+++ src/js/pages/game-room.js
@@ -1,23 +1,31 @@
 window.onload = function() {
-    (function($) {
-        var currentCallNumberElement = $('.current-call-number');
 
-        currentCallNumberElement.addClass('active');
+    (function() {
+        var ballRollingInterval,
+            gameBallsContainer = $('#game-balls-container'),
+            listOfNumbersElement = $('#number-caller-board__list-of-numbers'),
+            currentGameBalls = gameBallsContainer.find('#game-balls');
 
-        setInterval(function() {
-            var randomNumber = parseInt(Math.random() * 90 + 1);
-
-            currentCallNumberElement.removeClass('active');
-            setTimeout(function() {
-                currentCallNumberElement
-                    .attr('data-number', randomNumber)
-                    .addClass('active');
-
-                $('#number-caller-board__list-of-numbers>li.active')
+        window.noBallRolling = function() {
+            clearInterval(ballRollingInterval);
+            gameBallsContainer.removeClass('active');
+        };
+        window.startBallRolling = function() {
+            gameBallsContainer.addClass('active');
+            ballRollingInterval = setInterval(function() {
+                var randomNumber = parseInt(Math.random() * 90 + 1);
+                listOfNumbersElement
+                    .find('li.active')
                     .removeClass('active');
-                $('#number-caller-board__list-of-numbers>li[data-number="'+ randomNumber +'"]')
+                listOfNumbersElement
+                    .find('li[data-number="' + randomNumber + '"]')
                     .addClass('active');
-            }, 1400);
-        }, 4000);
+                console.log('Generated random number: ', randomNumber);
+                currentGameBalls
+                    .append('<li>' + randomNumber + '</li>');
+            }, 3000);
+        };
     })(jQuery);
+
+    window.startBallRolling();
 };

2b3aac1287797e6fad1f156a856dbe829540838a
belaevslava <belaevslava@gmail.com>
2018-03-02 21:36:58 +0200

    fixed game room style bugs

--- src/js/common.js
+++ src/js/common.js
@@ -1,18 +1,1 @@
-(function($) {
-    feather.replace();
-    AOS.init();
-
-    $('#carousel .carousel-item:eq(0)').addClass('active');
-
-    $('.carousel').carousel({
-        interval: false
-    });
-
-    $(".carousel").swipe({
-        swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
-            if (direction == 'left') $(this).carousel('next');
-            if (direction == 'right') $(this).carousel('prev');
-        },
-        allowPageScroll: "vertical"
-    });
-})(jQuery);
+feather.replace();
